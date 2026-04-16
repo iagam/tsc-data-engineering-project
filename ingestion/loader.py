79c43aa3c1c74a6aab23eaa36e5567d8cef7819c
Agam <54039021+iagam@users.noreply.github.com>
2026-04-16 11:08:58 +0530

    Refactor load_raw_data function to use batch load method

--- ingestion/loader.py
+++ ingestion/loader.py
@@ -4,38 +4,27 @@ import json
 
 def load_raw_data(client, dataset: str, run_id: str, data: dict, raw_table: str):
     """
-    Inserts raw JSON data into a BigQuery table with run_id and ingested_at timestamp.
+    Loads raw JSON data into BigQuery using batch load.
 
     Args:
-        client (google.cloud.bigquery.Client): BigQuery client instance
-        dataset (str): Dataset containing the target table
-        run_id (str/int): Unique identifier for the current pipeline run
-        data (dict): Raw data/JSON payload to be stored
-        raw_table (str): Destination table
-
-    Returns:
-        None
-
-    Raises:
-        Exception: If the BigQuery insert operation encounters errors, detailing the specific failure messages.
+        client: BigQuery client
+        dataset (str): Dataset name
+        run_id (str): Unique pipeline run ID
+        data (dict): API response
+        raw_table (str): Target table
     """
     table_id = f"{client.project}.{dataset}.{raw_table}"
 
     rows = [
         {
-            "run_id": run_id,
+            "run_id": 2,
             "ingested_at": datetime.now(timezone.utc).isoformat(),
-            "raw_json": json.dumps(data),
+            "raw_json": data,
         }
     ]
 
-    errors = client.insert_rows_json(table_id, rows)
-
-    if errors:
-        error_messages = []
-
-        for err in errors:
-            for e in err.get("errors", []):
-                error_messages.append(e.get("message", "Error Unknown"))
-
-        raise Exception(f"BigQuery insert failed: {error_messages}")
+    try:
+        job = client.load_table_from_json(rows, table_id)
+        job.result()
+    except Exception as e:
+        raise RuntimeError(f"BigQuery load failed: {str(e)}")
